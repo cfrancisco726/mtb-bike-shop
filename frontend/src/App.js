2e38651f6ab9418045233394e16330847b092d1e
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2022-02-03 21:51:57 -0500

    products displayed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
+import data from "./data";
+
 function App() {
   return (
     <div className="App">
       <header>
         <a href="/">mtb bike shop</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img
+                  className="bike-photo"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>{product.brand}</p>
+                <p>${product.price}</p>
+                <p>{product.year}</p>
+                <button>add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: "stump jumper",
+      model: "evo",
+      year: "2022",
+      build: "s works",
+      color: "black",
+      brand: "specialized",
+      category: "bikes",
+      price: "$10,000",
+      image: "/images/tallboy.png",
+      slug: "1",
+    },
+
+    {
+      name: "tallboy",
+      model: "C",
+      year: "2022",
+      build: "R",
+      brand: "Santa Cruz",
+      category: "bikes",
+      price: "5,000",
+      image: "/images/stump_jumper_evo_sworks.png",
+      slug: "2",
+    },
+    {
+      name: "stump jumper",
+      model: "evo",
+      year: "2022",
+      build: "s works",
+      color: "black",
+      brand: "specialized",
+      category: "bikes",
+      price: "10,000",
+      image: "/images/tallboy.png",
+      slug: "3",
+    },
+
+    {
+      name: "tallboy",
+      model: "C",
+      year: "2022",
+      build: "R",
+      brand: "Santa Cruz",
+      category: "bikes",
+      price: "$5,000",
+      image: "/images/stump_jumper_evo_sworks.png",
+      slug: "4",
+    },
+  ],
+};
+
+export default data;
