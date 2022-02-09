3295a10cac8778993a87d217a9eb14ff0d6b8bb1
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2022-02-08 20:31:22 -0500

    bcrypt pw works with rest client

--- backend/data.js
+++ backend/data.js
@@ -1,11 +1,26 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Carlo",
+      email: "cfrancisco26@yahoo.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Karen",
+      email: "karen@gmail.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: "stump jumper",
       model: "evo",
       year: "2022",
       build: "s works",
-      color: "black",
       brand: "specialized",
       category: "bikes",
       price: 7000,
@@ -28,32 +43,31 @@ const data = {
       image: "/images/stump_jumper_evo_sworks.png",
       slug: "2",
     },
-    // {
-    //   name: "stump jumper",
-    //   model: "evo",
-    //   year: "2020",
-    //   build: "s works",
-    //   color: "black",
-    //   brand: "specialized",
-    //   category: "bikes",
-    //   price: 10000,
-    //   countInStock: 10,
-    //   image: "/images/tallboy.png",
-    //   slug: "3",
-    // },
+    {
+      name: "stump jumper",
+      model: "evo",
+      year: "2020",
+      build: "s works",
+      brand: "specialized",
+      category: "bikes",
+      price: 10000,
+      countInStock: 10,
+      image: "/images/tallboy.png",
+      slug: "3",
+    },
 
-    // {
-    //   name: "tallboy",
-    //   model: "C",
-    //   year: "2019",
-    //   build: "R",
-    //   brand: "Santa Cruz",
-    //   category: "bikes",
-    //   price: 1000,
-    //   countInStock: 10,
-    //   image: "/images/stump_jumper_evo_sworks.png",
-    //   slug: "4",
-    // },
+    {
+      name: "tallboy",
+      model: "C",
+      year: "2019",
+      build: "R",
+      brand: "Santa Cruz",
+      category: "bikes",
+      price: 1000,
+      countInStock: 10,
+      image: "/images/stump_jumper_evo_sworks.png",
+      slug: "4",
+    },
   ],
 };
 
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils.js";
+import expressAsyncHandler from "express-async-handler";
+
+const userRouter = express.Router();
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    console.log("req", req.body.email);
+    // console.log("res", res);
+    const user = await User.findOne({ email: req.body.email });
+    console.log("user", user);
+    if (user) {
+      console.log("up", user.password);
+      console.log("req.body", req.body.password);
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+export default userRouter;
